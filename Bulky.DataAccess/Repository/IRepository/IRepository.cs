db5e4936c382730313965f80e39a7d6892d48fb3
Weng, Jeffrey <Jeffrey.Weng@wv.gov>
2026-01-29 12:17:23 -0500

    Add generic repository pattern with CRUD operations
    Changed IRepository interface to public for wider access. Added a generic Repository<T> class implementing IRepository<T> using Entity Framework Core, supporting Add, Get, GetAll, Remove, and RemoveRange methods for any entity. Enables reusable, type-safe data access across the application.

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    internal interface IRepository <T> where T : class
+    public interface IRepository <T> where T : class
     {
         //T - Category
         IEnumerable<T> GetAll();
