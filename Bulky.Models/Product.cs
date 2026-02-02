4266500c278a24404d4134e8238a619897cb3f05
Weng, Jeffrey <Jeffrey.Weng@wv.gov>
2026-02-02 10:55:49 -0500

    Add foreign key from Product to Category
    Introduced CategoryId and navigation property to Product model. Added migration to create foreign key, update seed data, and enforce referential integrity. Updated ApplicationDbContext and model snapshot to reflect relationship.

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Bulky.Models
@@ -38,7 +39,10 @@ namespace Bulky.Models
         [Range(1, 1000)]
         public double Price100 { get; set; }
 
+        public int CategoryId { get; set; }
 
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
 
     }
 }
