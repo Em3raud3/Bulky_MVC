9eae24f2538004bd7afeafa1b39ac001cb9d0988
Weng, Jeffrey <Jeffrey.Weng@wv.gov>
2026-02-04 10:56:25 -0500

    Add Product API endpoint and integrate DataTables support
    Added GetAll() API in ProductController to return products with categories as JSON. Integrated DataTables CSS/JS in _Layout.cshtml to enable advanced table features in the UI.

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -135,5 +135,17 @@ namespace BulkyWeb.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
+
+        //Make API CALLS region
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+
+        #endregion
     }
 }
