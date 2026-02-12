137a181cc52e9269696b058f296af6c9175b6a17
Weng, Jeffrey <Jeffrey.Weng@wv.gov>
2026-02-12 13:20:30 -0500

    Add ShoppingCart model, repository, and migration
    Introduced ShoppingCart entity with navigation to Product and ApplicationUser. Added EF Core migration for ShoppingCarts table with foreign keys and indexes. Extended repository pattern with IShoppingCartRepository and implementation. Updated UnitOfWork and ApplicationDbContext to support ShoppingCart. Removed unused IWebHostEnvironment from CompanyController.

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace Bulky.DataAccess.Repository.IRepository
         ICategoryRepository Category { get; }
         IProductRepository Product { get; }
         ICompanyRepository Company { get; }
+        IShoppingCartRepository ShoppingCart { get; }
 
         void Save();
     }
