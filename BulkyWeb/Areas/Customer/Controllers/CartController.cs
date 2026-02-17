e64f1d224d1c6b0d3bfa951bc87834073410c930
Weng, Jeffrey <Jeffrey.Weng@wv.gov>
2026-02-17 11:05:41 -0500

    Add cart order summary view and update navigation links
    Added Summary action and view for cart order summary and shipping details. Updated Index.cshtml to link to Summary. Removed hardcoded cart item count from navigation bar in _Layout.cshtml.

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -37,6 +37,12 @@ namespace BulkyWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary()
+        {
+       
+            return View(ShoppingCartVM);
+        }
+
         public IActionResult Plus(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
