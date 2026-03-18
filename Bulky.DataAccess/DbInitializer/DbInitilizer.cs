a0d80add0ddbfcb870b4396cfc7c98118339c044
Weng, Jeffrey <Jeffrey.Weng@wv.gov>
2026-03-18 15:18:23 -0400

    Add DbInitializer and IDbInitilizer for DB seeding
    Introduced IDbInitilizer interface with an Initialize() method and its DbInitializer implementation in Bulky.DataAccess.DbInitializer. This sets up the structure for database seeding functionality.

---
+++ Bulky.DataAccess/DbInitializer/DbInitilizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bulky.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitilizer
+    {
+        public void Initialize()
+        {
+            // Seed the database with initial data
+        }
+    }
+}
---
+++ Bulky.DataAccess/DbInitializer/IDbInitilizer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bulky.DataAccess.DbInitializer
+{
+    public interface IDbInitilizer
+    {
+        void Initialize();
+    }
+}
